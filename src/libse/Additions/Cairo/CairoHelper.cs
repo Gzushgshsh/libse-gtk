c6b93020fe49ed7e5dd475e423dcddaae3e8ca75
Lucas <lucaslourenco846@protonmail.com>
2025-04-08 22:04:26 -0300

    Add CairoHelper

---
+++ src/libse/Additions/Cairo/CairoHelper.cs
@@ -0,0 +1,42 @@
+using Cairo;
+using Gdk;
+using Point = Gdk.Point;
+
+namespace Lucas.SubtitleEdit.CairoExpansions
+{
+    public static class CairoHelper
+    {
+        /// <summary>
+        /// Draw the <paramref name="image"/> in <paramref name="tlCorner"/> whith shear effect
+        /// </summary>
+        /// <param name="cr">The context to use</param>
+        /// <param name="image">The Pixbuf to draw</param>
+        /// <param name="tlCorner">Top-Right corner location</param>
+        /// <param name="urCorner">Top-Left corner location</param>
+        /// <param name="dlCorner">Down-Left corner location</param>
+        private static void DrawImageShear(Context cr, Pixbuf image, Point tlCorner, Point urCorner, Point dlCorner)
+        {                
+            double xx, yx, xy, yy; 
+            
+            // Width scale
+            double width = urCorner.X - tlCorner.X;
+            xx = width / image.Width;
+            
+            // Height scale
+            double height = dlCorner.Y - tlCorner.Y;
+            yy = height / image.Height;
+
+            // Moving scale of the top right corner
+            yx = (urCorner.Y - tlCorner.Y) / height;
+            
+            // Moving scale of the down left corner
+            xy = (dlCorner.X - tlCorner.X) / width;
+            
+            cr.Save();
+            cr.Transform(new Matrix(xx, xy, yx, yy, tlCorner.X, tlCorner.Y));
+            Gdk.CairoHelper.SetSourcePixbuf(cr, image, 0, 0);
+            cr.Paint();
+            cr.Restore();
+        }
+    }
+}
